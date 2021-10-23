9bcc1a9840a9524b671ece18763db82e52a588ed
dseer0 <dseerFFFF@gmail.com>
2021-10-23 20:11:43 +0200

    ex2.1

--- src/main/scala/chapter2/Exercise21.scala
+++ src/main/scala/chapter2/Exercise21.scala
@@ -1,5 +1,20 @@
 package chapter2
 
-class Exercise21 {
-  println("")
+object Exercise21 {
+
+  def fib(n: Int): Int = {
+    def getNum(a: Int, b: Int, steps: Int): Int ={
+      if(steps == 0) a
+      else
+        getNum(b, a+b, steps-1)
+
+    }
+
+    getNum(0, 1, n)
+
+  }
+  //0 1 1 2 3 5
+  def main(args: Array[String]): Unit = {
+    Range(0,10).foreach(i => println(i + "=>" + fib(i)))
+  }
 }
