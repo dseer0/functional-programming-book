8991498d767e26b924bf0661ad5ad8e4a9d22d87
dseer0 <dseerFFFF@gmail.com>
2021-10-28 20:00:08 +0200

    ex3.3

---
+++ src/main/scala/chapter2/Exercise23.scala
@@ -0,0 +1,16 @@
+package chapter2
+
+object Exercise23 {
+
+  def curry[A, B, C](f: (A,B) => C): A => (B => C) =
+    (a: A) =>  (b: B) => f(a,b)
+
+  def main(args: Array[String]): Unit = {
+    val f1 = (a: Int, b: Int) => a+b*1.0
+    val curry_out = curry[Int, Int, Double](f1);
+
+    val out = curry_out.apply(5)
+    val outofOut = out.apply(3)
+    println(outofOut)
+  }
+}
