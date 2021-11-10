90a3655fbb5e271857397d2ab49c001b7106f4b7
dseer0 <dseerFFFF@gmail.com>
2021-11-10 21:11:58 +0100

    exercise3.3

--- src/main/scala/chapter3/test/Test1.scala
+++ src/main/scala/chapter3/test/Test1.scala
@@ -1,11 +1,14 @@
 package chapter3.test
 
 object Test1 {
+
   sealed trait List[+A]
+
   case object Nil extends List[Nothing]
+
   case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-  object List{
+  object List {
     def sum(ints: List[Int]): Int = ints match {
       case Nil => 0
       case Cons(x, xs) => x + sum(xs)
@@ -17,25 +20,29 @@ object Test1 {
       case Cons(x, xs) => x * product(xs)
     }
 
-    def tail[A](list: List[A]): List[A] ={
+    def tail[A](list: List[A]): List[A] = {
       list match {
         case Nil => Nil
         case Cons(x, xs) => xs
       }
     }
 
+    def setHead[A](list: List[A], value: A): List[A] = {
+      list match {
+        case Nil => Cons(value, Nil)
+        case Cons(x, xs) => Cons(value, Cons(x, xs))
+      }
+    }
+
     def apply[A](as: A*): List[A] =
-      if(as.isEmpty) Nil
+      if (as.isEmpty) Nil
       else Cons(as.head, apply(as.tail: _*))
   }
 
   def main(args: Array[String]): Unit = {
-    val z = List[Double](3,4,5)
+    val z: List[Int] = List.setHead(List(2,3, 4), 15)
     println(z)
-    println(List.tail(z))
-    println(List.tail(Nil))
-    println(List(1))
-    println(List.tail(List(1)))
+
   }
 
 }
