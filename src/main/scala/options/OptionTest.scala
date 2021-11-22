ae288e99979bf48090e159ec0d77fd9095b5b4b5
dseer0 <dseerFFFF@gmail.com>
2021-11-22 01:54:48 +0100

    variance

--- src/main/scala/options/OptionTest.scala
+++ src/main/scala/options/OptionTest.scala
@@ -28,16 +28,29 @@ object OptionTest {
     def filter(f: A => Boolean): Option[A] = {
       flatMap(a => if (f(a)) Some(a) else None)
     }
-
-
   }
 
   case class Some[+A](get: A) extends Option[A]
+
   case object None extends Option[Nothing]
 
+  object Option {
+    def variance(xs: Seq[Double]): Option[Double] = {
+      //m = mean
+      //mean(math.pow(a-m,2)
+      mean(xs)
+        .flatMap(m => mean(xs.map(el => math.pow(el - m, 2))))
+    }
+
+    def mean(xs: Seq[Double]): Option[Double] = {
+      if (xs.isEmpty) None
+      else Some(xs.sum / xs.size)
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-    val op = Some(Some(3))
-    val op2 = op.flatMap(a=>a)
+    val op = Some(Some(Some(Some(3))))
+    val op2 = op.flatMap(a => a).flatMap(a => a).flatMap(a => a)
     println(op2)
   }
 
